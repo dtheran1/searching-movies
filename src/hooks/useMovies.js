d8e0a33c1afc1af76de6b2ba8f41f3c6bae9a30a
Dtheran <dtheran1@gmail.com>
2023-02-16 18:40:07 -0500

    implement services and fetch

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,14 +1,13 @@
-import responseMovies from '../mocks/with-results.json'
+import { useState } from 'react'
+import { searchMovies } from '../services/movies'
 
-export function UseMovies () {
-  const movies = responseMovies.Search
+export function useMovies ({ search }) {
+  const [movies, setMovies] = useState([])
 
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    image: movie.Poster
-  }))
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search })
+    setMovies(newMovies)
+  }
 
-  return { movies: mappedMovies }
+  return { movies, getMovies }
 }
