62969453089e13883c609326dbdf32f360e85a83
Dtheran <dtheran1@gmail.com>
2023-02-16 19:05:25 -0500

    wip

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
@@ -6,9 +6,15 @@ export function useMovies ({ search }) {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
+  // Con este ref validamos no hacer las llamadas al api si es el mismo search del input anterior.
+  const previousSearch = useRef(search)
+
   const getMovies = async () => {
+    if (search === previousSearch.current) return
+
     try {
       setLoading(true)
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (error) {
